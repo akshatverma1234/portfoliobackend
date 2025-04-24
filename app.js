651fb94e671407fb049ab393f448d88072dfc73b
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2025-04-24 20:33:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB Connected"))
-  .catch((err) => console.error("MongoDB Error:", err))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected")).catch((err) => console.error("MongoDB Error:", err))
 
 // POST Route for Contact Form
 app.post("/api/contact", async (req, res) => {
